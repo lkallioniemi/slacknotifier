02dd1a57382b010e81198b4b4c4fc79ae3cc7809
Lauri Kallioniemi <lauri.kallioniemi@gmail.com>
2015-05-11 15:43:16 +0300

    temp

--- public/index.php
+++ public/index.php
@@ -3,18 +3,19 @@
         if ($_POST['token'] == 'Yo8DgpfdSOWBnNIYfzB4WbWw'){
             if ($_POST['command'] == "/ooo"){
                 $url = 'https://hooks.slack.com/services/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';
-                $data = array('payload' => 'value1', 'key2' => 'value2');
+                $data = array('payload' => '{"text": "This is a line of text in a channel.\nAnd this is another line of text."}');
 
                 // use key 'http' even if you send the request to https://...
                 $options = array(
                     'http' => array(
+                    'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
                     'method'  => 'POST',
-                    'payload' => '{"text": "This is a line of text in a channel.\nAnd this is another line of text."}';,
+                    'content' => http_build_query($data);,
                     ),
                 );
                 $context  = stream_context_create($options);
                 $result = file_get_contents($url, false, $context);
-                var_dump($context);
+                var_dump($content);
                 die($result);
             } else {
                 die('I don\'t know this command');
