57d96f4ab2c85cc961c168cd7d47cb63ef37bd82
Lauri Kallioniemi <lauri.kallioniemi@gmail.com>
2015-05-11 16:10:51 +0300

    temp

--- public/index.php
+++ public/index.php
@@ -8,10 +8,10 @@
                 curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
                 curl_setopt($ch,CURLOPT_HEADER, false); 
                 curl_setopt($ch, CURLOPT_POST, 1);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data, '', '&'););
+                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data, '', '&'));
                 $reply=curl_exec($ch);
                 curl_close($ch);
-                var_dump($context);
+                var_dump($reply);
             } else {
                 die('I don\'t know this command');
             }
