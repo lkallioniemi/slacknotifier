1fa828e78a4252daf396154564a1c3fa6739b22f
Lauri Kallioniemi <lauri.kallioniemi@gmail.com>
2015-05-11 15:31:35 +0300

    temp

--- public/index.php
+++ public/index.php
@@ -8,13 +8,13 @@
                 // use key 'http' even if you send the request to https://...
                 $options = array(
                     'http' => array(
-                    'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
                     'method'  => 'POST',
-                    'content' => http_build_query($data),
+                    'payload' => '{"text": "This is a line of text in a channel.\nAnd this is another line of text."}';,
                     ),
                 );
                 $context  = stream_context_create($options);
                 $result = file_get_contents($url, false, $context);
+                var_dump($content);
                 die($result);
             } else {
                 die('I don\'t know this command');
