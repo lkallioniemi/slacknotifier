59ac23dd4ea9a3bace43ef92e814c332519ac404
Lauri Kallioniemi <lauri.kallioniemi@gmail.com>
2015-05-11 16:41:14 +0300

    temp

--- public/temp.php
+++ public/temp.php
@@ -1,5 +1,5 @@
 <?php 
-                $data = array('payload' => '{"text": '.$_POST['text'].'", "username": "Nurce Jackie", "icon_url": "https://slack.com/img/icons/app-57.png", "icon_emoji": ":hospital:"}');
+                $data = array('payload' => '{"text": "This is a line of text in a channel.\nAnd this is another line of text.", "username": "Nurce Jackie", "icon_url": "https://slack.com/img/icons/app-57.png", "icon_emoji": ":hospital:"}');
                 $ch = curl_init();
                 curl_setopt($ch,CURLOPT_URL,"https://hooks.slack.com/services/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");
                 curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
@@ -8,5 +8,5 @@
                 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data, '', '&'));
                 $reply=curl_exec($ch);
                 curl_close($ch);
-                var_dump($context);
+                var_dump($reply);
 ?>
